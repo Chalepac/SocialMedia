7a91db80e843abfee385cef0536e9c8422a43797
c.arango <c.arango@visiontravel.net>
2020-10-09 17:18:44 -0500

    RESTFul API with .NET Core 3.1 - Part 6 - Data Transfer Objects and Automapper

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,7 +30,14 @@ namespace SocialMedia.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            //Configuramos el automapper
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            //Agregamos NewtonsoftJson 
+            services.AddControllers().AddNewtonsoftJson(option =>
+            {
+                option.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
 
             //Se configura el contexto de la BD con la cadena de conexion correspondiente
             services.AddDbContext<SocialMediaContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SocialMediaConnection")));
