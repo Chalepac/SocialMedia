f0db5c4cb538fd4d15a504524c8c5a009551e082
carango <carango@heyprimo.com>
2022-08-11 18:12:56 -0500

    Update 11/08/2022

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -43,8 +43,8 @@ namespace SocialMedia.Api
             services.AddDbContext<SocialMediaContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SocialMediaConnection")));
 
             services.AddTransient<IPostService, PostService>();
-            services.AddTransient<IPostRepository, PostRepository>();
-            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
 
             //Validaciones (Fluent Validation)
             services.AddMvc(option =>
---
+++ SocialMedia.infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SocialMediaContext _Context;
+        private readonly IRepository<Post> _postRepository;
+        private readonly IRepository<User> _userRepository;
+        private readonly IRepository<Comment> _CommentRepository;
+
+        public UnitOfWork(SocialMediaContext context)
+        {
+            _Context = context;
+        }
+
+        public IRepository<Post> PostRepository => _postRepository ?? new BaseRepository<Post>(_Context);
+
+        public IRepository<User> UserRepository => _userRepository ?? new BaseRepository<User>(_Context);
+
+        public IRepository<Comment> CommentRepository => _CommentRepository ?? new BaseRepository<Comment>(_Context);
+
+        public void Dispose()
+        {
+            if(_Context != null)
+            {
+                _Context.Dispose();
+            }
+        }
+
+        public void SaveChanges()
+        {
+            _Context.SaveChangesAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _Context.SaveChangesAsync();
+        }
+    }
+}
